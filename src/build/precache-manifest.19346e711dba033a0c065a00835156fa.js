a2bf404a2a959b701deca93a12665d8bd49c4620
Justas <karalevicius@gmail.com>
2020-02-11 12:56:17 +0000

    First Commit

---
+++ src/build/precache-manifest.19346e711dba033a0c065a00835156fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe2964393c6cd786984288829dc6273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02a27ac6e7a0f8741a13",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "b18924a6dbc6c341baf1",
+    "url": "/static/js/2.a6e815c1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a6e815c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a27ac6e7a0f8741a13",
+    "url": "/static/js/main.8a9a69ec.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
